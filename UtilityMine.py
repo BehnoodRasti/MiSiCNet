7fdb61762dab61bc05ee1ea8fd05041ced8d2eb8
Behnood Rasti <61419984+BehnoodRasti@users.noreply.github.com>
2022-01-29 16:25:59 +0100

    Add files via upload

--- UtilityMine.py
+++ UtilityMine.py
@@ -1,18 +1,86 @@
-from utils.denoising_utils import *
+#from utils.denoising_utils import *
 from numpy import linalg as LA
 import numpy as np
-from skimage._shared import *
-from skimage.util import *
-from skimage.metrics.simple_metrics import _as_floats
-from skimage.metrics.simple_metrics import mean_squared_error
-from skimage._shared.utils import * 
-from pymf.dist import l1_distance
+#from skimage._shared import *
+#from skimage.util import *
+#from skimage.metrics.simple_metrics import _as_floats
+#from skimage.metrics.simple_metrics import mean_squared_error
+#from skimage._shared.utils import * 
+#from pymf.dist import l1_distance
 dtype_range = {np.bool_: (False, True),
                np.bool8: (False, True),
                np.float16: (-1, 1),
                np.float32: (-1, 1),
                np.float64: (-1, 1)}
 import torch
+import torch.nn as nn
+
+def fill_noise(x, noise_type):
+    """Fills tensor `x` with noise of type `noise_type`."""
+    if noise_type == 'u':
+        x.uniform_()
+    elif noise_type == 'n':
+        x.normal_() 
+    else:
+        assert False
+
+def get_noise(input_depth, method, spatial_size, noise_type='u', var=1./10):
+    """Returns a pytorch.Tensor of size (1 x `input_depth` x `spatial_size[0]` x `spatial_size[1]`) 
+    initialized in a specific way.
+    Args:
+        input_depth: number of channels in the tensor
+        method: `noise` for fillting tensor with noise; `meshgrid` for np.meshgrid
+        spatial_size: spatial size of the tensor to initialize
+        noise_type: 'u' for uniform; 'n' for normal
+        var: a factor, a noise will be multiplicated by. Basically it is standard deviation scaler. 
+    """
+    if isinstance(spatial_size, int):
+        spatial_size = (spatial_size, spatial_size)
+    if method == 'noise':
+        shape = [1, input_depth, spatial_size[0], spatial_size[1]]
+        net_input = torch.zeros(shape)
+        
+        fill_noise(net_input, noise_type)
+        net_input *= var            
+    elif method == 'meshgrid': 
+        assert input_depth == 2
+        X, Y = np.meshgrid(np.arange(0, spatial_size[1])/float(spatial_size[1]-1), np.arange(0, spatial_size[0])/float(spatial_size[0]-1))
+        meshgrid = np.concatenate([X[None,:], Y[None,:]])
+        net_input=  torch.from_numpy(meshgrid)
+    else:
+        assert False
+        
+    return net_input
+
+
+def get_params(opt_over, net, net_input, downsampler=None):
+    '''Returns parameters that we want to optimize over.
+
+    Args:
+        opt_over: comma separated list, e.g. "net,input" or "net"
+        net: network
+        net_input: torch.Tensor that stores input `z`
+    '''
+    opt_over_list = opt_over.split(',')
+    params = []
+    
+    for opt in opt_over_list:
+    
+        if opt == 'net':
+            params += [x for x in net.parameters() ]
+        elif  opt=='down':
+            assert downsampler is not None
+            params = [x for x in downsampler.parameters()]
+        elif opt == 'input':
+            net_input.requires_grad = True
+            params += [net_input]
+        else:
+            assert False, 'what is it?'
+            
+    return params
+
+
+
 def compare_snr(image_true, image_test, *, data_range=None):
     """
     Compute the signal to noise ratio (PSNR) for an image.
